6335da43bd6062033b41c27cb03e46019fc42a59
a0106 <a0106@xogns>
2025-12-04 01:09:15 +0900

    5번 (방향 추가)

--- zuul/src/w13/Game.java
+++ zuul/src/w13/Game.java
@@ -19,7 +19,7 @@ public class Game {
 	 * Create all the rooms and link their exits together. 방들을 만들고 방의 출구들을 서로 엮어준다.
 	 */
 	private void createRooms() {
-		Room hall, lectureRoom, computerRoom, office, dongBang;
+		Room hall, lectureRoom, computerRoom, office, dongBang, cellar;
 
 		// create the rooms
 		hall = new Room("Hall");
@@ -27,6 +27,7 @@ public class Game {
 		dongBang = new Room("Dongari room");
 		computerRoom = new Room("Computer room");
 		office = new Room("Office");
+		cellar = new Room("Cellar");
 
 		// initialize room exits
 		hall.setExit("east", lectureRoom);
@@ -39,8 +40,11 @@ public class Game {
 		
 		computerRoom.setExit("north", hall);
 		computerRoom.setExit("east", office);
+		computerRoom.setExit("down", cellar);
 		
 		office.setExit("west", computerRoom);
+		
+		cellar.setExit("up", computerRoom);
 
 		currentRoom = hall; // 홀에서 게임을 시작한다.
 	}
